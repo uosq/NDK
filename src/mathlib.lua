d88ddc4a11a234ecf847f2acf4fc63669158da81
uosq <lboxwallytf2@gmail.com>
2025-12-18 03:32:56 -0300

    Improve mathlib

--- src/mathlib.lua
+++ src/mathlib.lua
@@ -1,5 +1,7 @@
 local lib = {}
 
+local RAD2DEG = 180/math.pi
+
 ---@param vec Vector3
 ---@return number
 function lib.NormalizeVector(vec)
@@ -15,16 +17,42 @@ function lib.NormalizeVector(vec)
 	return len
 end
 
----@param a Vector3
----@param b Vector3
+---@param from Vector3
+---@param to Vector3
+---@param clamp boolean # When true, clamps the angle (default: true)
+function lib.CalcAngle(from, to, clamp)
+	clamp = clamp == nil and true or clamp
+
+	local delta = from - to
+	local hyp = math.sqrt(delta.x^2 + delta.y^2)
+
+	local angle = Vector3(
+		math.atan(delta.z/hyp) * RAD2DEG,
+		math.atan(delta.y / delta.x) * RAD2DEG,
+		0
+	)
+
+	--- this is stupid
+	if clamp == true then
+		angle = Vector3(vector.AngleNormalize(angle):Unpack())
+	end
+
+	return angle
+end
+
+---@param a EulerAngles
+---@param b EulerAngles
 ---@return number
-function lib.Fov(a, b)
-	return math.acos(a:Dot(b)) * (180 / math.pi)
+function lib.CalcFov(a, b)
+	local aForward, bForward
+	aForward = a:Forward()
+	bForward = b:Forward()
+	return math.acos(aForward:Dot(bForward)) * RAD2DEG
 end
 
 function lib.DirectionToAngles(direction)
-    local pitch = math.asin(-direction.z) * (180 / math.pi)
-    local yaw = math.atan(direction.y, direction.x) * (180 / math.pi)
+    local pitch = math.asin(-direction.z) * RAD2DEG
+    local yaw = math.atan(direction.y, direction.x) * RAD2DEG
     return Vector3(pitch, yaw, 0)
 end
 
@@ -46,8 +74,8 @@ function lib.SolveBallisticArc(p0, p1, speed, gravity)
 	end
 
 	local angle = math.atan((speed2 - math.sqrt(root)) / (g * dx)) -- low arc
-	local yaw = (math.atan(diff.y, diff.x)) * (180 / math.pi)
-	local pitch = -angle * (180 / math.pi)
+	local yaw = (math.atan(diff.y, diff.x)) * RAD2DEG
+	local pitch = -angle * RAD2DEG
 
 	return Vector3(pitch, yaw, 0)
 end
